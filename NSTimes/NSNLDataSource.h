e3f041a8fe2714cb4244d807fcee0d1045cd0426
Robert Dougan <rdougan@me.com>
2012-12-10 16:54:31 +0100

    Adding NSNLDataSource

---
+++ NSTimes/NSNLDataSource.h
@@ -0,0 +1,14 @@
+//
+//  NSNLDataSource.h
+//  NSTimes
+//
+//  Created by Simon Maddox on 06/12/2012.
+//  Copyright (c) 2012 Robert Dougan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSRailConnection.h"
+
+@interface NSNLDataSource : NSObject <NSRailConnectionDataSource>
+
+@end
--- NSTimes/NSRailConnection.h
+++ NSTimes/NSRailConnection.h
@@ -8,12 +8,40 @@
 
 #import <Foundation/Foundation.h>
 
+@class Train, TFHppleElement;
+
+@protocol NSRailConnectionDataSource <NSObject>
+
+- (NSArray *)stations;
+- (NSString *)defaultDepartureStation;
+- (NSString *)defaultArrivalStation;
+
+@optional
+
+- (NSURLRequest *)requestWithFrom:(NSString *)from to:(NSString *)to;
+- (NSURLRequest *)requestForMoreWithFrom:(NSString *)from to:(NSString *)to;
+
+- (NSString *)XPathQueryForTrains;
+
+- (NSDate *)train:(Train *)train departureDateFromElement:(TFHppleElement *)element;
+- (NSDate *)train:(Train *)train arrivalDateFromElement:(TFHppleElement *)element;
+- (NSString *)train:(Train *)train platformFromElement:(TFHppleElement *)element;
+- (NSString *)train:(Train *)train travelTimeFromElement:(TFHppleElement *)element;
+- (NSString *)train:(Train *)train departureDelayFromElement:(TFHppleElement *)element;
+- (NSString *)train:(Train *)train arrivalDelayFromElement:(TFHppleElement *)element;
+- (BOOL)shouldDisplayTrain:(Train *)train;
+
+// Implement this if you want to provide your own parsing implementation
+- (NSArray *)trainsWithData:(NSData *)data;
+
+@end
+
 @interface NSRailConnection : NSObject
 
 @property (nonatomic, assign) NSString *from;
 @property (nonatomic, assign) NSString *to;
 
-@property (nonatomic, retain) NSArray *stations;
+@property (nonatomic, strong) id <NSRailConnectionDataSource> dataSource;
 
 + (NSRailConnection *)sharedInstance;
 
@@ -21,5 +49,13 @@
 
 - (void)fetchWithSuccess:(void (^)(NSArray *trains))success failure:(void (^)(NSError *error))failure;
 - (void)fetchMoreWithSuccess:(void (^)(NSArray *trains))success failure:(void (^)(NSError *error))failure;
+
+#pragma mark - Normalization
+
+- (NSDate *)dateForString:(NSString *)string;
+- (NSString *)normalizeString:(NSString *)string;
+
+#pragma mark - Stations
+- (NSArray *)stations;
 
 @end
